54be440272fa77759d2beec9b35bd61bcb1b05d7
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2023-03-09 11:36:52 +0300

    auth controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+/* eslint-disable import/no-named-as-default */
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
 
-const server = express();
+export default class AuthController {
+  static async getConnect(req, res) {
+    const { user } = req;
+    const token = uuidv4();
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
+  }
 
-export default server;
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
+  }
+}
